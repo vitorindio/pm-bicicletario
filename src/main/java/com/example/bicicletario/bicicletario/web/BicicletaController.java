6da4e029f2843543812d066aaaf128dcc3b5ea29
Rafael Ventura <raf4ael@gmail.com>
2024-07-02 22:36:06 -0300

    tentativa de adicionar o swagger ao projeto

--- src/main/java/com/example/bicicletario/bicicletario/web/BicicletaController.java
+++ src/main/java/com/example/bicicletario/bicicletario/web/BicicletaController.java
@@ -1,39 +1,26 @@
 package com.example.bicicletario.bicicletario.web;
 
-import com.example.bicicletario.bicicletario.application.BicicletaService;
-import com.example.bicicletario.bicicletario.domain.enums.Status;
 import com.example.bicicletario.bicicletario.domain.Bicicleta;
+import com.example.bicicletario.bicicletario.domain.enums.Status;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+@Tag(name = "Bicicletas", description = "Operações relacionadas a bicicletas")
 public class BicicletaController {
 
-
-    private final BicicletaService bicicletaService;
-
-    public BicicletaController(BicicletaService bicicletaService) {
-        this.bicicletaService = bicicletaService;
-    }
-
-    @GetMapping("/bicicletas")
+    @Operation(summary = "Lista todas as bicicletas")
+    @GetMapping("/api/bicicletas")
     public List<Bicicleta> listarBicicletas() {
-        return List.of(new Bicicleta("marca", "modelo", "2021", 1, Status.DISPONIVEL));
-
+        return List.of(
+                new Bicicleta("Caloi", "Vermelha", "2020", 1, Status.DISPONIVEL),
+                new Bicicleta("Monark", "Azul", "2021", 2, Status.DISPONIVEL)
+        );
     }
 
-    @PostMapping("/bicicletas")
-    public Bicicleta criarBicicleta(Bicicleta bicicleta) {
-
-        return bicicletaService.criarBicicleta(bicicleta);
-    }
 
 }
-
-
-
